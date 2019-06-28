ee4afa5fb9aa0307e3be4afd478ea042fe788f31
Manny Ferrario <vferrario@rccl.com>
2019-06-28 15:18:40 -0400

    files

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build -t  monitor-service  .
+sudo docker build -t  monitor-service  .
+sudo docker run --rm -it --network bridge --rm -p 9889:9889 \
+-v "$(pwd)"/logs:/logs  \
+--name monitor-service_dev monitor-service:latest
+echo "done"
